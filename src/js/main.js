b85da2602644dc64c60651dbecfcdef61523d917
dpmango <xs290@me.com>
2016-12-17 23:17:01 +0300

    mainpage sidebar javascript + hamburger

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
+  // hamburger
+  var click = 1;
+  $("#show-me-menu").on("click", clickHamb);
+  function clickHamb() {
+      if ( click == 1 ) {
+          $(this).addClass('is-active');
+          $('html, body, .st-container, .st-pusher').css('height', "100%");
+          //$('.mobile-nav').slideToggle(300);
+          click = 2;
+      } else {
+      $(this).removeClass('is-active');
+      $('html, body, .st-container, .st-pusher').css('height', "auto");
+      //$('.mobile-nav').hide();
+          click = 1;
+      }
+  }
+
+  // Parallax and scrolling
+  $(window).scroll(function(){
+    var headerTop = $('.header').offset().top;
+    var scrollTop = $(window).scrollTop();
+
+    if( scrollTop >= 500 ){
+      $('.header').addClass('header--floating');
+    } else {
+      $('.header').removeClass('header--floating');
+    }
+  });
+
 
  	// Prevent # errors
 	$('[href="#"]').click(function (e) {
