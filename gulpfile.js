577f9b34d90c77ea63b5813e1077543a0592b693
dpmango <xs290@me.com>
2016-12-19 02:38:13 +0300

    responsive draft

--- gulpfile.js
+++ gulpfile.js
@@ -64,8 +64,8 @@ var processors = [
     assets( {loadPaths: ['./src/images'], relative: true } ),
     svginline(),
     autoprefixer({browsers: ['> 1%', 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']}),
-    sorting(),
-    mqpacker(),
+    // sorting(),
+    // mqpacker(),
     pixrem(),
     //cssnano(),
 ];
